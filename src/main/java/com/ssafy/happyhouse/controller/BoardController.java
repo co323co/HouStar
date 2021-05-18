ee21d2ae6d2ea0147254e62c6a60fc676c850ad3
김민정 <co323co@gmail.com>
2021-05-18 18:38:50 +0900

    board : 게시글 id로 get 수정

--- src/main/java/com/ssafy/happyhouse/controller/BoardController.java
+++ src/main/java/com/ssafy/happyhouse/controller/BoardController.java
@@ -31,52 +31,31 @@ public class BoardController {
 	@Autowired
 	BoardService bSer;
 
-//	@ApiOperation("게시판 gubun으로 해당 게시판의 모든 게시글을 반환, word가 있을 경우 제목에 word가 포함된 게시글만 반환한다")
-//	@GetMapping({"/{gubun}", "/{gubun}/{word}"})
-//	@ResponseBody
-//	public List<BoardDto> listById(@PathVariable String gubun, @PathVariable(required=false) String word) {
-//		Map<String, String> map = new HashMap<>();
-//		System.out.println(word);
-//		map.put("gubun", gubun);
-//		map.put("word", word);
-//		return bSer.listByBid(map);
-//	}
-//	
+	@ApiOperation("게시판 gubun으로 해당 게시판의 모든 게시글을 반환, word가 있을 경우 제목에 word가 포함된 게시글만 반환한다")
+	@GetMapping({"/{gubun}", "/{gubun}/{word}"})
+	@ResponseBody
+	public List<BoardDto> listById(@PathVariable String gubun, @PathVariable(required=false) String word) {
+		Map<String, String> map = new HashMap<>();
+		System.out.println(word);
+		map.put("gubun", gubun);
+		map.put("word", word);
+		return bSer.listByBid(map);
+	}
+	
 	@ApiOperation("gubun(pk)으로 해당 게시판의 이름을 반환한다.")
 	@GetMapping("name/{gubun}")
 	@ResponseBody
 	public String findNameByGubun(@PathVariable int gubun) {
 		return bSer.findNameByGubun(gubun);
 	}
-	
-	@ApiOperation("게시판 gubun으로 해당 게시판의 모든 게시글을 반환")
-	@GetMapping("/{gubun}")
-	@ResponseBody
-	public List<BoardDto> listById(@PathVariable String gubun) {
-		Map<String, String> map = new HashMap<>();
-		map.put("gubun", gubun);
-		return bSer.listByBid(map);
-	}
-
-	@ApiOperation("게시판 gubun으로 해당 게시판의 모든 게시글을 반환, word가 있을 경우 제목에 word가 포함된 게시글만 반환한다")
-	@GetMapping("/{gubun}/{word}")
-	@ResponseBody
-	public List<BoardDto> listById(@PathVariable String gubun, @PathVariable(required=false) String word) {
-		Map<String, String> map = new HashMap<>();
-		map.put("gubun", gubun);
-		map.put("word", word);
-		return bSer.listByBid(map);
-	}
 
 	@ApiOperation("게시글 id로 해당 게시글에 대한 정보를 얻어온다")
 	@GetMapping("post/{id}")
-	public ModelAndView view(@PathVariable("id") int id) {
-		ModelAndView mv = new ModelAndView("board/notice_view");
+	public BoardDto view(@PathVariable("id") int id) {
 		BoardDto board = bSer.select(id);
 		board.setViews(board.getViews()+1);	//조회수 1 올려줌
 		bSer.update(board);
-		mv.addObject("board", board);
-		return mv;
+		return board;
 	}
 
 	
