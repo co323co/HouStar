3ef04877299e50ae97ae6a1b15fa52f7268344c5
김민정 <co323co@gmail.com>
2021-05-19 12:37:13 +0900

    댓글컨트롤러에 crossorigin 달아놓음

--- src/main/java/com/ssafy/happyhouse/controller/CommentController.java
+++ src/main/java/com/ssafy/happyhouse/controller/CommentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import io.swagger.annotations.ApiOperation;
 
 @Api("댓글(Comment) API")
 @RestController
+@CrossOrigin("*")
 @RequestMapping("api/comment")
 public class CommentController {
 
