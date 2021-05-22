6ef52564d7f7aa0357041046154624a63416b4d2
김채연 <tomybaru@naver.com>
2021-05-22 18:24:03 +0900

    dongselectbar commit

--- src/components/aboutdong/DongSelectBar.vue
+++ src/components/aboutdong/DongSelectBar.vue
@@ -1,39 +1,61 @@
 <template lang="">
-  <div>
-    <!-- 시군구 select -->
-    <div class="bg-dark section pt-4 pb-4">
-      <div class="sorting-filters text-center mb-20 d-flex justify-content-center">
-        <div class="form-group mr-2">
-          <select
-            class="form-control bgPink"
-            name="sido"
-            id="sido"
-            @change="getGugun()"
-            v-model="sido"
-          >
-            <option value="" disabled selected hidden>선택</option>
-            <option v-for="(s, idx) in sidos" :key="idx" :value="s">{{ s.sidoName }}</option>
-          </select>
-        </div>
-        <div class="form-group md-5">
-          <select class="form-control" name="gugun" id="gugun" v-model="gugun" @change="getDong()">
-            <option value="" disabled selected hidden>선택</option>
-            <option v-for="(g, idx) in guguns" :key="idx" :value="g">{{ g.gugunName }}</option>
-          </select>
-        </div>
-        <div class="form-group ml-2">
-          <select class="form-control" name="dong" id="dong" v-model="dong" @change="moveInfo()">
-            <option value="" disabled selected hidden>선택</option>
-            <option v-for="(d, idx) in dongs" :key="idx" :value="d">{{ d.dongName }}</option>
-          </select>
+  <v-app>
+    <div>
+      <!-- 시군구 select -->
+      <div class="bg-secondary section pt-2 pb-2">
+        <div class="sorting-filters text-center mt-10 d-flex justify-content-center">
+          <div class="form-group mr-2">
+            <v-container>
+              <v-row dense>
+                <v-col cols="4">
+                  <v-autocomplete
+                    v-model="sido"
+                    :items="sidos"
+                    item-text="sidoName"
+                    item-value="sidoCode"
+                    label="시 선택"
+                    dense
+                    solo
+                    @change="getGugun()"
+                  ></v-autocomplete>
+                </v-col>
+                <v-col cols="4">
+                  <v-autocomplete
+                    v-model="gugun"
+                    :items="guguns"
+                    item-text="gugunName"
+                    item-value="gugunCode"
+                    label="구군 선택"
+                    dense
+                    solo
+                    @change="getDong()"
+                    return-object
+                  ></v-autocomplete>
+                </v-col>
+                <v-col cols="4">
+                  <v-autocomplete
+                    v-model="dong"
+                    :items="dongs"
+                    item-text="dongName"
+                    item-value="dongCode"
+                    label="동 선택"
+                    dense
+                    solo
+                    @change="moveInfo()"
+                    return-object
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </v-app>
 </template>
 <script>
 import http from '@/core/services/http-common';
-
+// import Vuetify from 'vuetify';
 export default {
   data() {
     return {
@@ -60,7 +82,7 @@ export default {
     getGugun() {
       // console.log(this.sido);
       http
-        .get('/address/guguns/' + this.sido.sidoCode)
+        .get('/address/guguns/' + this.sido)
         .then(({ data }) => {
           console.log(data);
           this.guguns = data;
@@ -71,6 +93,8 @@ export default {
         });
     },
     getDong() {
+      // console.log(this.)
+      // console.log(this.gugun);
       http
         .get('/address/dongs/' + this.gugun.gugunCode + '/' + this.gugun.gugunName)
         .then(({ data }) => {
@@ -85,10 +109,17 @@ export default {
     },
     moveInfo() {
       console.log(this.dong);
-      $store.commit('dongStore/setSidogugundong', this.dong);
-      console.log($store.state.Sidogugundong);
+      //$store.commit('dongStore/setSidogugundong', this.dong);
+      console.log(this.$store.state.dongStore.Sidogugundong);
+      this.$store.state.dongStore.Sidogugundong = this.dong;
+      console.log(this.$store.state.dongStore.Sidogugundong.dongName);
+      this.$router.push('dong-info');
     },
   },
 };
 </script>
-<style lang=""></style>
+<style>
+.v-list {
+  background-color: white;
+}
+</style>
