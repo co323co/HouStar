ab462ac5fde72c9784f27b27d0a966f257083ad5
김민정 <co323co@gmail.com>
2021-05-24 06:26:26 +0900

    동 추천 구현 완료

---
+++ src/components/ranking/SuggestDong.vue
@@ -0,0 +1,191 @@
+<template>
+  <v-layout justify-center>
+    <v-card class="pa-9 mt-7 mb-3" elevation="2" shaped width="80%">
+      <h2>추천 동네</h2>
+      <!-- 사용자 정보 -->
+      <div class="user_info mt-2">
+        <h5>{{ currentUser.tag | list }}</h5>
+        <h5 v-if="currentUser.age_range" class="mx-2">|</h5>
+        <h5>{{ currentUser.age_range | ageRange }}</h5>
+        <h5 v-if="currentUser.family_type" class="mx-2">|</h5>
+        <h5>{{ currentUser.family_type | familyType }}</h5>
+      </div>
+      <v-divider></v-divider>
+      <!-- 동 랭킹 -->
+      <v-row class="rank mt-1 mx-1">
+        <v-col v-if="suggest_dong.age_range">
+          <h6 class="mb-2">{{ currentUser.age_range | ageRange }} 취향저격 순위</h6>
+          <div v-for="(dong, idx) in suggest_dong.age_range" :key="idx" @click="moveInfo(dong)">
+            <MouseOver :msg="icon_rank[idx] + ' ' + dong.dongName" />
+          </div>
+        </v-col>
+        <v-col v-if="suggest_dong.family_type">
+          <h6 class="mb-2">{{ currentUser.family_type | familyType }} 취향저격 순위</h6>
+          <div v-for="(dong, idx) in suggest_dong.family_type" :key="idx" @click="moveInfo(dong)">
+            <MouseOver :msg="icon_rank[idx] + ' ' + dong.dongName" />
+          </div>
+        </v-col>
+        <v-col v-if="suggest_dong.tag">
+          <h6 class="mb-2">{{ currentUser.tag | list }} 취향저격 순위</h6>
+          <div v-for="(dong, idx) in suggest_dong.tag" :key="idx" @click="moveInfo(dong)">
+            <MouseOver :msg="icon_rank[idx] + ' ' + dong.dongName" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+import http from '@/core/services/http-common';
+import { mapGetters } from 'vuex';
+import MouseOver from '@/components/ranking/MouseOverText.vue';
+
+export default {
+  components: {
+    MouseOver,
+  },
+  data() {
+    return {
+      suggest_dong: {
+        age_range: null,
+        family_type: null,
+        tag: null,
+      },
+      icon_rank: ['🥇', '🥈', '🥉'],
+    };
+  },
+  mounted() {
+    this.setSuggestDongs();
+  },
+  computed: {
+    ...mapGetters(['currentUser']),
+  },
+  filters: {
+    ageRange: function (ageRange) {
+      if (!ageRange) return '';
+      if (ageRange == 'over') return '60대 이상';
+      else return ageRange + '대';
+    },
+    familyType: function (type) {
+      if (!type) return '';
+      else return type;
+    },
+    list: function (list) {
+      if (!list) return '';
+      let str = '';
+      for (let i = 0; i < list.length - 1; i++) {
+        str += list[i] + '     ·     ';
+      }
+      str += list[list.length - 1];
+      return str;
+    },
+  },
+  methods: {
+    setSuggestDongs() {
+      //로그인 유저의 연령대가 높게 평가한 동네 TOP3를 suggest_dong.age_range에 설정
+
+      //로그인 유저의 연령대가 평가한 동 평점 리스트 받아오기
+      http
+        .get(`/dongreview/avg-rating/type?age_range=${this.currentUser.age_range}`)
+        .then((rating_response) => {
+          let rating_list = rating_response.data;
+          // total 점수를 기준으로 내림차순 정렬
+          rating_list.sort(function (a, b) {
+            return b.total - a.total;
+          });
+          //   console.log('age_range : ', rating_list);
+          this.suggest_dong.age_range = [];
+          //탑 3개까지 뽑음
+          for (let i = 0; i < 5; i++) {
+            //동코드로 SigugundongDto 찾아서 suggest_dong.age_range에 넣음, (사용자의 연령대로 찾은 추천 동 목록)
+            if (rating_list.length <= i) return;
+            http.get('/address/' + rating_list[i].dongcode).then((dong_resonse) => {
+              this.suggest_dong.age_range.push(dong_resonse.data);
+            });
+          }
+        });
+
+      //로그인 유저와 같은 가구타입이  높게 평가한 동네 TOP3를 suggest_dong.family_type에 설정
+
+      //로그인 유저의 가구타입이 평가한 동 평점 리스트 받아오기
+      http
+        .get(`/dongreview/avg-rating/type?family_type=${this.currentUser.family_type}`)
+        .then((rating_response) => {
+          let rating_list = rating_response.data;
+          // total 점수를 기준으로 내림차순 정렬
+          rating_list.sort(function (a, b) {
+            return b.total - a.total;
+          });
+          //   console.log('family_type : ', rating_list);
+          this.suggest_dong.family_type = [];
+          //탑 3개까지 뽑음
+          for (let i = 0; i < 5; i++) {
+            //동코드로 SigugundongDto 찾아서 suggest_dong.family_type 넣음, (사용자의 가구타입으로 찾은 추천 동 목록)
+            if (rating_list.length <= i) return;
+            http.get('/address/' + rating_list[i].dongcode).then((dong_resonse) => {
+              this.suggest_dong.family_type.push(dong_resonse.data);
+            });
+          }
+        });
+
+      //로그인 유저가 선호하는 '타입들의 총합'이 높은 TOP3를  suggest_dong.tag에 넣음 (사용자의 선호태그로 찾은 추천 동 목록)
+
+      //모든 동의 평점 정보 리스트 받아오기
+      http.get('/dongreview/avg-rating').then((rating_response) => {
+        let rating_list = rating_response.data;
+        // '유저의 선호태그들'만으로 '평점 합'을 구하고, 그걸 기준으로 내림차순 정렬
+        let user_tag = this.currentUser.tag;
+        rating_list.sort(function (a, b) {
+          let a_sum = 0;
+          let b_sum = 0;
+          //   해당 동의 태그들의 총 평점 합 구하기
+          for (let tag of user_tag) {
+            if (tag == '인프라') {
+              a_sum += a.infra * 1.0;
+              b_sum += b.infra * 1.0;
+            } else if (tag == '대중교통') {
+              a_sum += a.trans * 1.0;
+              b_sum += b.trans * 1.0;
+            } else if (tag == '안전') {
+              a_sum += a.safety * 1.0;
+              b_sum += b.safety * 1.0;
+            } else if (tag == '건강') {
+              a_sum += a.health * 1.0;
+              b_sum += b.health * 1.0;
+            } else if (tag == '학군') {
+              a_sum += a.school * 1.0;
+              b_sum += b.school * 1.0;
+            } else if (tag == '환경') {
+              a_sum += a.environment * 1.0;
+              b_sum += b.environment * 1.0;
+            }
+          }
+          return b_sum - a_sum;
+        });
+        // console.log('tag : ', rating_list);
+        this.suggest_dong.tag = [];
+        //탑 3개까지 뽑음
+        for (let i = 0; i < 5; i++) {
+          //동코드로 SigugundongDto 찾아서 suggest_dong.tag 넣음, (사용자의 선호타입으로 찾은 추천 동 목록)
+          if (rating_list.length <= i) return;
+          http.get('/address/' + rating_list[i].dongcode).then((dong_resonse) => {
+            this.suggest_dong.tag.push(dong_resonse.data);
+          });
+        }
+      });
+    },
+    moveInfo(dongDto) {
+      //   console.log(this.$store.state.dongStore.Sidogugundong);
+      this.$store.state.dongStore.Sidogugundong = dongDto;
+      //   console.log(this.$store.state.dongStore.Sidogugundong.dongName);
+      this.$router.push('dong-info');
+    },
+  },
+};
+</script>
+<style scoped>
+.user_info h5 {
+  display: inline-block;
+}
+</style>
--- src/view/pages/Home.vue
+++ src/view/pages/Home.vue
@@ -1,35 +1,13 @@
 <template lang="">
   <v-container class="justify-center">
-    <v-layout>
-      <v-carousel
-        v-ripple
-        hide-delimiter-background
-        delimiter-icon="mdi-minus"
-        show-arrows-on-hover="true"
-        height="500"
-        cycle="cycle"
-        interval="4000"
-        tag="#tag"
-      >
-        <v-carousel-item
-          v-for="(item, i) in banner"
-          :key="i"
-          :src="item.src"
-          reverse-transition="fade-transition"
-          transition="fade-transition"
-        >
-          <v-flex id="banner-text">
-            <div style="height: 75%"></div>
-            <h1>Houstar</h1>
-            <h3>Looking for a place to love?</h3>
-          </v-flex>
-        </v-carousel-item>
-      </v-carousel>
-    </v-layout>
+    <!-- 배너 사진 띄우는 레이아웃 -->
+    <Banner />
+    <!-- 추천 동네 레이아웃 -->
+    <SuggestDong />
+    <!-- 동 셀렉스 박스 레이아웃 -->
     <v-layout>
       <v-flex>
-        <br />
-        <h2 class="mb-5">동네 구경하기</h2>
+        <h2 class="my-5">동네 구경하기</h2>
         <DongSelectBar />
       </v-flex>
     </v-layout>
@@ -37,29 +15,13 @@
 </template>
 <script>
 import DongSelectBar from '@/components/aboutdong/DongSelectBar.vue';
+import SuggestDong from '@/components/ranking/SuggestDong.vue';
+import Banner from '@/view/layout/Banner.vue';
+
 export default {
-  data() {
-    return {
-      banner: [
-        {
-          src: 'media/banner/house1.jpg',
-        },
-        {
-          src: 'media/banner/house2.jpg',
-        },
-        {
-          src: 'media/banner/house3.jpg',
-        },
-        {
-          src: 'media/banner/house4.jpg',
-        },
-        {
-          src: 'media/banner/house5.jpg',
-        },
-      ],
-    };
-  },
   components: {
+    Banner,
+    SuggestDong,
     DongSelectBar,
   },
 };
@@ -67,12 +29,5 @@ export default {
 <style scoped>
 h2 {
   text-align: center;
-}
-#banner-text {
-  color: beige;
-  margin: auto;
-  text-align: center;
-  height: 100%;
-  text-shadow: -1px 0 black, 0 1px black, 1px 0 lightgrey, 0 -1px lightgrey;
 }
 </style>
